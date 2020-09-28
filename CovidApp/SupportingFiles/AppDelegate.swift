1318b326205834c6b76ef6b8ca323c50bc17ab2d
Irakli Shelia <i.shelia@bog.ge>
2020-09-28 12:57:24 +0400

    Refactor

--- CovidApp/SupportingFiles/AppDelegate.swift
+++ CovidApp/SupportingFiles/AppDelegate.swift
@@ -16,7 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window?.overrideUserInterfaceStyle = .light
+        
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound]) {(granted, error) in
+                // Make sure permission to receive push notifications is granted
+                print("Permission is granted: \(granted)")
+            }
+        
         return true
     }
 }
